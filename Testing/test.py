cf4b443373ea025a8110aca46c944b6989931463
AnikoNEXT <anzs@nextkbh.dk>
2023-07-19 15:44:46 +0200

    Test

---
+++ Testing/test.py
@@ -0,0 +1,18 @@
+print ("Hello Bello")
+
+
+#print("Hi! This is a double quote mark: " We use it for printing.")
+
+print("Hi! This is a double quote mark: \" We use it for printing.")
+
+print("The file is stored in C:\new folder")
+
+"""
+This is a sample program to show how to draw using the Python programming
+language and the Arcade library.
+
+Multi-line comments are surrounded by three double-quote marks.
+Single-line comments start with a hash/pound sign. #
+"""
+
+# This is a single-line comment.
