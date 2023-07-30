b76d4138342532550a853dae53592634c43e36f2
AnikoNEXT <anzs@nextkbh.dk>
2023-07-30 11:42:02 +0200

    Test

--- Lab 05 - Loopy Lab/lab_05.py
+++ Lab 05 - Loopy Lab/lab_05.py
@@ -1,3 +1,12 @@
+class Person:
+    def __init__(self):
+        self.name: str = "A"
+
+
+mary = Person()
+mary.name = 22
+print(mary.name)
+
 # Copy of the array to modify
 my_list = [5, 76, 8, 5, 3, 3, 56, 5, 23]
 
@@ -21,3 +30,34 @@ for item in my_list:
 
 # Print the result
 print(my_list)
+
+#x = "This is a sample string"
+x = "0123456789"
+
+print("x=", x)
+
+
+class Person:
+    def __init__(self):
+        self.name: str = "A"
+
+
+mary = Person()
+mary.name = 22
+print(mary)
+
+# Accessing the first character ("T")
+print("x[0]=", x[0])
+
+# Accessing the second character ("h")
+print("x[1]=", x[1])
+
+# Accessing from the right side ("g")
+print("x[-1]=", x[-1])
+
+# Access 0-5 ("This ")
+print("x[:6]=", x[:6])
+# Access 6 to the end ("is a sample string")
+print("x[6:]=", x[6:])
+# Access 6-8
+print("x[6:9]=", x[6:9])
